c82c1698a8998e8c1b224be40af601eec6b4d12f
ahadsenja <hamdanilm96@gmail.com>
2021-05-05 03:36:30 +0800

    feat: add dashboard and notes component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotesComponent } from './notes/notes.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'notes', component: NotesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
