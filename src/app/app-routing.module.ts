9ef37b111fbae65b48890848abdfa01fff319966
ahadsenja <hamdanilm96@gmail.com>
2021-05-07 19:00:05 +0800

    feat: add edit note component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotesAddComponent } from './components/notes-add/notes-add.component';
+import { NotesEditComponent } from './components/notes-edit/notes-edit.component';
 import { NotesComponent } from './components/notes/notes.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'notes', component: NotesComponent }
+  { path: 'notes', component: NotesComponent },
+  { path: 'notes/add', component: NotesAddComponent },
+  { path: 'notes/edit', component: NotesEditComponent },
 ];
 
 @NgModule({
