7df80a874e9c7754d28580864d38682d204edc53
ahadsenja <hamdanilm96@gmail.com>
2021-05-07 17:31:03 +0800

    chore: update get all note method

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Note } from '../models/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+  private URL = 'http://localhost:3000/notes';
+  
+  notes: Note[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  onGetAll(): Observable<Note[]> {
+    return this.http.get<Note[]>(this.URL);
+  }
 }
