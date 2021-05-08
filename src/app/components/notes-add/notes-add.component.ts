0e8520f6d18cca5fa2b7f9abda395bf19c7d2f19
ahadsenja <hamdanilm96@gmail.com>
2021-05-08 08:20:20 +0800

    feat: add router navigation after submit data

--- src/app/components/notes-add/notes-add.component.ts
+++ src/app/components/notes-add/notes-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { NoteService } from 'src/app/services/note.service';
+import { Note } from 'src/app/models/note';
 
 @Component({
   selector: 'app-notes-add',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesAddComponent implements OnInit {
 
-  constructor() { }
+  notes: Note[] = [];
+
+  id: number = 0;
+  title: string = '';
+  content: string = '';
+  submitted = false;
+
+  constructor(
+    private noteService: NoteService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onCreateNote(): void {
+    const data = {
+      id: this.id,
+      title: this.title,
+      content: this.content
+    }
+    this.noteService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        }
+      )
+    
+      this.router.navigate(['/notes']);
   }
 
 }
