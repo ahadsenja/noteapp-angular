fde7874fec7d34fe9c975f9c621fcdf365d1d34d
ahadsenja <hamdanilm96@gmail.com>
2021-05-10 15:23:14 +0800

    feat: pass note data parameter and add onUpdate method

--- src/app/components/notes-edit/notes-edit.component.ts
+++ src/app/components/notes-edit/notes-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Note } from '../../models/note';
+import { NoteService } from 'src/app/services/note.service';
 
 @Component({
   selector: 'app-notes-edit',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes-edit.component.css']
 })
 export class NotesEditComponent implements OnInit {
+  notes: Note[] = [];
+  note: Note = new Note();
 
-  constructor() { }
+  id: number = 0;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private noteService: NoteService
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.noteService.getNoteById(this.id)
+      .subscribe(data => {
+        this.note = data
+        console.log(data)
+      })
+  }
+
+  onUpdate() {
+    this.noteService.update(this.id, this.note)
+      .subscribe(data => {
+        console.log(data)
+        this.router.navigate(['/notes'])
+      }, error => console.log(error))
   }
 
 }
