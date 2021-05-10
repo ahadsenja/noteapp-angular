b7ef94feb211f3c6c3aabc1d846aec9a82402e3b
ahadsenja <hamdanilm96@gmail.com>
2021-05-10 15:25:35 +0800

    feat: add onSelectNote method to get data from server

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { Note } from 'src/app/models/note';
@@ -12,7 +13,10 @@ import { NoteService } from 'src/app/services/note.service';
 export class NotesComponent implements OnInit {
   notes: Note[] = [];
 
-  constructor(private noteService: NoteService) { }
+  constructor(
+    private noteService: NoteService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.onGetNotes();
@@ -29,6 +33,10 @@ export class NotesComponent implements OnInit {
           console.log(error)
         }
       )
+  }
+
+  onSelectNote(_id: number): void {
+    this.router.navigate(['/notes/edit', _id]);
   }
 
   onDelete(note: Note): void {
